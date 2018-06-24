567c5652881cfb62795c6bb90dfe43538263ce73
trdougherty <trd4@me.com>
2018-06-23 19:19:55 -0500

    Cleaning up averaging and display functions

--- divergence.py
+++ divergence.py
@@ -6,47 +6,35 @@ import numpy as np
 import argparse
 import io
 import sys
-import datetime
+import datetime 
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 
 #Variables of interest
-axis = 1
+axis = 0
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-o", "--output", required=False,
 	help="path to output video file")
-ap.add_argument("-i", "--input", required=False,
-        help="path to input file")
 args = vars(ap.parse_args())
-
-def makeResult(self, grayFrame, flow):
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    self.hsv[...,0] = ang*180/np.pi/2
-    self. hsv[...,2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    return cv2.cvtColor(self.hsv, cv2.COLOR_HSV2BGR)
-
-def main():
-    t0 = time.time()
-    name = args["input"]
-    photo = cv2.imread(name)
 
 def process(file):
     numObj = np.load(file)
-    [date, iter, ar1, ar2] = numObj
+    [date, iteration, ar1, ar2] = numObj
     recompose = np.zeros([ar1.shape[0],ar1.shape[1],2])
     recompose[:,:,0] = ar1
     recompose[:,:,1] = ar2
-    return [date, iter, recompose]
+    return [date, iteration, recompose]
 
-def localize(point, x, y, mv = 0.08):
+# Not really needed for this
+def localize(point, x, y, mv = 0.08): 
     Z = 1/np.power(np.power(point[0]-x,2)+np.power(point[1]-y,2),0.5)
     Z[ Z > mv ] = mv
     return Z*(1/mv)
 
 # This will give use the divergence of the array which we can use for localizing later
-def divergence(array): return np.gradient(array) #np.add.reduce(np.gradient(array))
+def gradient(array): return np.gradient(array) #np.add.reduce(np.gradient(array))
 
 def humanDate(ts):
     return datetime.datetime.fromtimestamp(ts)
@@ -55,34 +43,52 @@ def fileImport():
     imported = sys.stdin.read()
     return imported.rstrip().split('\n')
 
-if __name__ == '__main__':
-    files = fileImport()
-    [sumIter, sumArray] = process(files[0])[1:] #This gives us our first iterations and sum array
-    for i in files[1:]:
-        [iter, arr] = process(i)[1:]
-        sumIter += iter
-        sumArray += arr
-
-    x_dist = np.arange(0,sumArray.shape[0])
-    y_dist = np.arange(0,sumArray.shape[1])
+def printArr(arr, axis):
+    # Allows us to work with the shape off the photo we're looking at
+    x_dist = np.arange(0,arr.shape[0])
+    y_dist = np.arange(0,arr.shape[1])
 
     xx, yy = np.meshgrid(x_dist, y_dist)
-    ##print(divergence(sumArray)[2][10])
-    A = divergence(sumArray[:,:,axis])
-    Z = np.dot(A[axis],sumArray[:,:,axis])
+    Z = arr[:,:,axis]
+
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.contour3D(xx, yy, Z, 50, cmap='binary') #'binary')
-    ax.view_init(elev=27., azim=-44)
+    ax.contour3D(xx, yy, Z, arr.shape[0], cmap='binary')
     ax.set_title('{} Axis'.format(axis))
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     plt.show()
-    # print(distance(3,10,sumArray))
-    #distArr = np.zeros(sumArray.shape[0],sumArray.shape[1])
-    #for i in sumArray.shape[0]:
-    #    for j in sumArray.shape[1]:
-    #        distArr[i][j] =
+    return
 
-    #finalArr = sumArray / sumIter #This will give the final equivalent motion vector of the system
+
+# Shouldn't really be needed
+def normalize(x):
+    return x/np.amax(np.absolute(x))
+
+def saveArr(x):
+    assert x.shape[2] == 2 #Test to verify we're passing in the correct motion vectors
+    t = datetime.datetime.now().timestamp()
+    arrayStorage = np.array([t,x[:,:,0],x[:,:,1]], dtype = object)
+    np.save(args["output"], arrayStorage)
+
+
+if __name__ == '__main__':
+    files = fileImport()
+    dates = []
+    [date, sumIter, sumArray] = process(files[0]) #This gives us our first iterations and sum array
+    dates.append(date)
+
+    for i in files[1:]:
+        [temp_date, ite, arr] = process(i)
+        dates.append(temp_date)
+        sumIter += ite
+        sumArray += arr
+
+    motionArr = sumArray / sumIter #KEY LINE
+
+    # Allows us to work with the shape off the photo we're looking at
+    printArr(motionArr, axis)
+
+    #Finally saves the array
+    saveArr(motionArr)
