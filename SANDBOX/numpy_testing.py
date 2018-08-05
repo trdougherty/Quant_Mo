4299461c3fa4415d399aab0dc9a65ff91cfaa4c3
trdougherty <trdougherty@utexas.edu>
2018-08-05 12:03:02 -0500

    updating error prop for motion system

--- SANDBOX/numpy_testing.py
+++ SANDBOX/numpy_testing.py
@@ -11,10 +11,17 @@ def build_rand(inter):
         l.append(np.random.randint(0,255,(64,64,2)))
     return np.array(l,dtype='uint8')
 
-@profile
-def straw(array):
-    return array
+def avg(arg):
+    arr = np.array(arg)
+    return u.ufloat(np.average(arr),np.std(arr))
+
+def convert(arr):
+    assert type(arr).__module__ == np.__name__ # This confirms we're working with a numpy array
+    assert len(arr.shape) >= 3 # This confirms we're working with at least a 4D array
+    a = unumpy.uarray(np.sum(arr,axis=0)/arr.shape[0],np.std(arr,axis=0))
+    return a
 
 if __name__ == "__main__":
     m = build_rand(sys.argv[len(sys.argv)-1])
-    print(m[:,1,1,1].shape)
+    out = convert(m)
+    print(out.shape)
