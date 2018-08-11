34d2ae28455450613d9fe38cbc95f0b306f73428
trdougherty <trdougherty@utexas.edu>
2018-08-11 00:01:42 -0500

    Fixing optical flow pattern

--- flow.py
+++ flow.py
@@ -6,6 +6,6 @@ class Flow:
         self.prev = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     def calc(self, img):
         current = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        self.flow = cv2.calcOpticalFlowFarneback(self.prev, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        self.flow = cv2.calcOpticalFlowFarneback(self.prev, current, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         self.prev = current
         return self.flow
