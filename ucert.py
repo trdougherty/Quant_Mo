ccd6174381ba1834d90593957031504ceb0c544d
trdougherty <trdougherty@utexas.edu>
2018-08-10 23:58:54 -0500

    Removing profiling system for preformance measurement

--- ucert.py
+++ ucert.py
@@ -1,8 +1,6 @@
-from memory_profiler import profile
 from uncertainties import unumpy
 import uncertainties as u
 
-@profile
 def uncert(arr):
     assert type(arr).__module__ == np.__name__ # This confirms we're working with a numpy array
     assert len(arr.shape) >= 3 # This confirms we're working with at least a 4D array
