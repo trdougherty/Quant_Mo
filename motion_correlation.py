fff1ccb2f614c2c95c8f0479c2e98e687c8a3740
trdougherty <trdougherty@utexas.edu>
2019-01-20 18:19:09 +0800

    adding more automated command line visualization

--- motion_correlation.py
+++ motion_correlation.py
@@ -1,25 +1,7 @@
 # from __future__ import print_function
-import time
 import numpy as np
-import argparse
-import io
-import os
-import sys
-import datetime
-import matplotlib as mpl
-from matplotlib import pyplot as plt
-from matplotlib import animation
-import matplotlib.gridspec as gridspec
-from mpl_toolkits import mplot3d
-from uncertainties import unumpy
-import uncertainties as u
-import itertools
-import seaborn as sns
 import pandas as pd
-from tqdm import tqdm
-import scipy
-import supplement as sp
-import scipy.ndimage as ndimage
+import sys
 
 ## This is specific to this machine and experiment, should be replaced with config file if published
 experiment = '/Users/TRD/Research_Personal/Quant_Mo/data/aw_motion/'
@@ -33,7 +15,10 @@ photos = experiment + 'photos'
 filt = experiment + 'filt'
 
 # Hour of interest here:
-hour = 7
+if len(sys.argv) > 2:
+    hour = sys.argv[2]
+else:
+    raise
 h = str(hour)
 f_name = filt+'/'+h+e_name+'.npy'
 l_name = light_h+h+l_nam+'.npy'
--- study_motion.sh
+++ study_motion.sh
@@ -1,1 +1,5 @@
-bash motion_correlation.py 
+python motion_correlation.py 0 $1 > light.txt
+python motion_correlation.py 1 $1 > motion.txt
+
+hist -x -f light.txt
+hist -x -f motion.txt
