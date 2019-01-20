c502bd7a8da7c7ce45298a2a2123c61c09bebdf1
trdougherty <trdougherty@utexas.edu>
2019-01-20 22:56:23 +0800

    adding some files for cloud analysis

--- study_motion.sh
+++ study_motion.sh
@@ -1,5 +1,6 @@
 python motion_correlation.py 0 $1 > light.txt
 python motion_correlation.py 1 $1 > motion.txt
 
-hist -x -f light.txt
-hist -x -f motion.txt
+hist -b 150 -x -f light.txt
+hist -b 150 -x -f motion.txt
+scatter -x motion.txt -y light.txt -s 70
