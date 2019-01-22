278bc94eebe71c7640032b2228f0e5ce44a3699a
trdougherty <trdougherty@utexas.edu>
2019-01-22 09:11:08 +0800

    updating formatting

--- study_motion.sh
+++ study_motion.sh
@@ -1,6 +1,6 @@
 python motion_correlation.py 0 $1 > light.txt
 python motion_correlation.py 1 $1 > motion.txt
 
-hist -b 150 -x -f light.txt
-hist -b 150 -x -f motion.txt
+hist -b 60 -x -f light.txt
+hist -b 60 -x -f motion.txt
 scatter -x motion.txt -y light.txt -s 70
