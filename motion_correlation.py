64bf41b2024975950322a4005613ffe5187ecb2c
trdougherty <trdougherty@utexas.edu>
2019-01-23 17:21:17 +0800

    updating the data synthesis process

--- motion_correlation.py
+++ motion_correlation.py
@@ -2,6 +2,11 @@
 import numpy as np
 import pandas as pd
 import sys
+from scipy.ndimage.filters import gaussian_filter
+import matplotlib.pyplot as plt
+import seaborn as sns
+import supplement as spp
+from scipy import stats
 
 ## This is specific to this machine and experiment, should be replaced with config file if published
 experiment = 'data/aw_motion/'
@@ -10,31 +15,47 @@ light_h = 'data/elles/filt/'
 e_name = 'aw_motion'
 l_nam = 'aw_light'
 data = experiment + 'data'
-pickled = experiment + 'pickled'
+pickled = 'data/pickled'
 photos = experiment + 'photos'
 filt = experiment + 'filt'
 
 # Hour of interest here:
-if len(sys.argv) > 2:
-    hour = sys.argv[2]
+if len(sys.argv) > 1:
+    hour = sys.argv[1]
+    print("Hour is: {}".format(hour))
 else:
-    raise
+    hour = 8
+
 h = str(hour)
 f_name = filt+'/'+h+e_name+'.npy'
+p_name = pickled+'/'+h+e_name+'.pkl'
 l_name = light_h+h+l_nam+'.npy'
+
 M = np.load(f_name); # This should always work if the line before was run
 L = np.load(l_name); L = np.rot90(L); # This rot is to make the images follow the same pattern
 
 # This is looking at the raw influence of light on the motion of the frame
-data_setup = {'light':L.flatten(), 'motion':np.sum(np.absolute(M), axis=2).flatten()}
+light_red = gaussian_filter(np.rot90(L, k=3).astype(int), sigma=3)
+motion_red = np.sum(np.absolute(M), axis=2)
+
+# This is trimming our analysis to the good data
+front = 0.3; back = 0.74
+light_red = spp.edge(light_red, (front, back))
+motion_red = spp.edge(motion_red, (front, back))
+
+# This is looking at how the change in values is related to the other values
+light_change = np.gradient(light_red)
+motion_change = np.gradient(motion_red)
+
+data_setup = {'light':light_red.flatten(), 'light_dx':light_change[0].flatten(), 'light_dy':light_change[1].flatten(), 'raw_motion':motion_red.flatten(), 'motion_dx':motion_change[0].flatten(), 'motion_dy':motion_change[1].flatten()}
+
 df = pd.DataFrame(data=data_setup)
+
+# This next line removes outliers
+df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+df.to_pickle(p_name)
+
 df_corr = df.corr() # This is going to give us a correlation matrix to play with
-if len(sys.argv) > 1:
-    if str(sys.argv[1]) == '0':
-        print(df.light.to_string(index=False))
-    else:
-        print(df.motion.to_string(index=False))
-else:
-    pass
+print(df_corr.values.shape)
 
 #print(df_corr)
--- supplement.py
+++ supplement.py
@@ -5,15 +5,22 @@ from tqdm import tqdm
 import os
 
 def edge(arr, scope):
-    if len(scope) == 2:
+    if len(arr.shape) == 2:
+        x_ = arr.shape[0]; y_ = arr.shape[1]
         lowerBound_x = lowerBound_y = scope[0]
         upperBound_x = upperBound_y = scope[1]
-    elif len(scope) == 4:
-        lowerBound_x = scope[0]; lowerBound_y = scope[2]
-        upperBound_x = scope[1]; upperBound_y = scope[3]
-    else: return None
-    x_ = arr.shape[0]; y_ = arr.shape[1]
-    return arr[int(x_*lowerBound_x):int(x_*upperBound_x),int(y_*lowerBound_y):int(y_*upperBound_y),:]
+        return arr[int(x_*lowerBound_x):int(x_*upperBound_x),int(y_*lowerBound_y):int(y_*upperBound_y)]
+    else:
+        if len(scope) == 2:
+            lowerBound_x = lowerBound_y = scope[0]
+            upperBound_x = upperBound_y = scope[1]
+        elif len(scope) == 4:
+            lowerBound_x = scope[0]; lowerBound_y = scope[2]
+            upperBound_x = scope[1]; upperBound_y = scope[3]
+        else: return None
+        x_ = arr.shape[0]; y_ = arr.shape[1]
+        return arr[int(x_*lowerBound_x):int(x_*upperBound_x),int(y_*lowerBound_y):int(y_*upperBound_y),:]
+        
 
 def pythag(arr):
     # This is going to reduce the system into one array ~ ideally ~ a mxm matrix of rank 2
